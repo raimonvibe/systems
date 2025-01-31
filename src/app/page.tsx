7fc653319cf0e7f4348c5f49ac1ea1f69a8d5269
raimonvibe <robertjanstefan@gmail.com>
2025-01-31 10:43:01 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,6 +91,9 @@ export default function Home() {
 
         <div className="spacing"></div>
 
+
+        <div className="middle-container">
+ 
         <article>
             <div>
               <h1 className="centre">So, have a look around!</h1>
@@ -99,6 +102,7 @@ export default function Home() {
               </p>
             </div>
           </article>
+          </div>
     
 
         <div className="spacing"></div>
