1a5c6bcaa707c3c353aea21281e806eb3baaa274
raimonvibe <robertjanstefan@gmail.com>
2025-05-23 01:02:49 +0200

    Update page.tsx

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -171,7 +171,7 @@ export default function Home() {
 
           <div className="middle-container">
         <Image
-          className="common-properties common-properties-left"
+          className="common-properties common-properties-right"
           src={tetris}
           alt="A stylized, minimalistic visual of a Tetris game interface, set in warm neutral tones. The design showcases falling tetrominoes, a score panel, and level indicators, all within a calm, design-focused workspace."
         />
@@ -185,10 +185,27 @@ export default function Home() {
         </article>
       </div>
 
+        <div className="middle-container">
+        <Image
+          className="common-properties common-properties-left"
+          src={periodic}
+          alt="An interactive periodic table rendered in warm, minimal tones. Elements are color-coded and neatly arranged, radiating a sense of calm structure and scientific beauty."
+        />
+        <article>
+          <div>
+            <h1>The Periodic Table.</h1>
+            <p className="github">
+              Not just rows and columns — it’s a quiet map of everything. Each element has a story: iron in our blood, carbon in our breath, gold in our dreams. This project gives shape to that wonder. You click, you learn, and slowly, you start to see the order in the chaos. It’s about connection. And how everything — even us — fits in.
+            </p>
+          </div>
+        </article>
+      </div>
+
+
 
         <div className="middle-container">
           <Image
-            className="common-properties common-properties-left"
+            className="common-properties common-properties-right"
             src={welcome}
             alt="A serene and minimalist workspace featuring a desk with a computer displaying abstract designs, surrounded by neutral-toned decor, bookshelves, and warm lighting for a calming atmosphere."
           /> 
