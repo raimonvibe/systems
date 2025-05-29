162782b1ee4b4abb9802582d84ef6fb6df1db268
raimonvibe <robertjanstefan@gmail.com>
2025-05-29 02:00:41 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -36,7 +36,7 @@ const Footer: React.FC = () => {
     window.addEventListener("scroll", scrollFunction);
 
     if (mybutton) {
-      mybutton.style.color = "#F8F1F1";
+      mybutton.style.color = "#F8F1F1"; // This seems to be a default style, kept as is.
       mybutton.addEventListener("click", topFunction);
     }
 
@@ -50,51 +50,53 @@ const Footer: React.FC = () => {
 
   const socialLinks = [
     { href: "https://x.com/raimonvibe/", icon: faXTwitter, label: "Link to Twitter" },
-    { href: "https://www.youtube.com/channel/UCDGDNuYb2b2Ets9CYCNVbuA/videos/", icon: faYoutube, label: "Link to YouTube", className: "footer-link2" },
+    { href: "https://www.youtube.com/channel/UCDGDNuYb2b2Ets9CYCNVbuA/videos/", icon: faYoutube, label: "Link to YouTube", className: "c-footer-link--offset-right" },
     { href: "https://www.tiktok.com/@raimonvibe/", icon: faTiktok, label: "Link to TikTok" },
     { href: "https://www.instagram.com/raimonvibe/", icon: faInstagram, label: "Link to Instagram" },
     { href: "https://medium.com/@raimonvibe/", icon: faMedium, label: "Link to Medium" },
-    { href: "https://github.com/raimonvibe/", icon: faGithub, label: "Link to GitHub", className: "footer-link3" },
-    { href: "https://www.linkedin.com/in/raimonvibe/", icon: faLinkedin, label: "Link to LinkedIn", className: "footer-link3" },
-    { href: "https://www.facebook.com/profile.php?id=61563450007849", icon: faFacebook, label: "Link to Facebook", className: "footer-link3" },
+    { href: "https://github.com/raimonvibe/", icon: faGithub, label: "Link to GitHub", className: "c-footer-link--offset-left" },
+    { href: "https://www.linkedin.com/in/raimonvibe/", icon: faLinkedin, label: "Link to LinkedIn", className: "c-footer-link--offset-left" },
+    { href: "https://www.facebook.com/profile.php?id=61563450007849", icon: faFacebook, label: "Link to Facebook", className: "c-footer-link--offset-left" },
   ];
 
   return (
-    <footer className="container-fluid">
-      {socialLinks.map((link, index) => (
-        <React.Fragment key={link.href}>
-          {index % 2 === 0 && index !== 0 && <div className="spacing4" />}
-          <Link
-            href={link.href}
-            className={`footer-link ${link.className || ''}`}
-            target="_blank"
-            rel="noreferrer"
-            aria-label={link.label}
-          >
-            <FontAwesomeIcon icon={link.icon} size="1x" />
-          </Link>
-        </React.Fragment>
-      ))}
-      <div className="spacing4" />
+    <footer className="o-container--fluid">
+      <div className="c-footer-icons">
+        {socialLinks.map((link, index) => (
+          <React.Fragment key={link.href}>
+            {index % 2 === 0 && index !== 0 && <div className="u-spacing-xs" />}
+            <Link
+              href={link.href}
+              className={`c-social-icon ${link.className ? link.className.replace('footer-link', 'c-footer-link') : ''}`}
+              target="_blank"
+              rel="noreferrer"
+              aria-label={link.label}
+            >
+              <FontAwesomeIcon icon={link.icon} size="1x" />
+            </Link>
+          </React.Fragment>
+        ))}
+      </div>
+      <div className="u-spacing-xs" />
       <Link
         href="/PrivacyNotice"
-        className="footer-link footer-link2"
-        aria-label="Link to Privacy Policy"
+        className="c-footer-link c-footer-link--offset-right"
+        aria-label="Link to Privacy Notice"
       >
         Privacy Notice
       </Link>
-      <div className="spacing4" />
+      <div className="u-spacing-xs" />
       <Link
         href="/LegalNotice"
-        className="footer-link footer-link2"
+        className="c-footer-link c-footer-link--offset-left"
         aria-label="Link to Legal Notice"
       >
         Legal Notice
       </Link>
-      <p className="footer-link">
+      <p className="c-copyright">
         <Link
           href="https://www.raimonvibe.com/"
-          className="footer-link"
+          className="c-footer-link"
           target="_blank"
           rel="noreferrer"
           aria-label="Link to Raimonvibe Homepage"
@@ -102,7 +104,7 @@ const Footer: React.FC = () => {
           © {new Date().getFullYear()} raimonvibe
         </Link>
       </p>
-      <button id="myBtn" title="Go to top">
+      <button id="myBtn" className="c-btn c-btn--back-to-top" title="Go to top" aria-label="Scroll back to top of page">
         Back to Top
       </button>
     </footer>
